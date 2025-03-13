db6b61ecc0ce696912f17d08eac778660abdbfee
lxizaxi <USERNAME@users.noreply.github.com>
2025-03-13 23:22:17 +0900

    hoge

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from deutsch-jozsa-algo!")
+import marimo
+
+__generated_with = "0.11.19"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    return
 
 
 if __name__ == "__main__":
-    main()
+    app.run()
